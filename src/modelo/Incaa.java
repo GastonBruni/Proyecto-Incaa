d82da2aa3aceffca8d8d1f787e4d194b27af18c3
Gaston <Gastonbrunijavier@hotmail.com>
2020-09-02 20:05:37 -0300

    Completo las clases con sus variables correspondientes.
    Declaro las variables como Privadas ya que en el DER estan declaradas con el signo -

--- src/modelo/Incaa.java
+++ src/modelo/Incaa.java
@@ -1,5 +1,42 @@
 package modelo;
 
+import java.util.List;
+
 public class Incaa {
+	private List<Pelicula> lstPeliculas;
+
+	public Incaa(List<Pelicula> lstPeliculas) {
+		super();
+		this.lstPeliculas = lstPeliculas;
+	}
+
+	public List<Pelicula> getLstPeliculas() {
+		return lstPeliculas;
+	}
+
+	public void setLstPeliculas(List<Pelicula> lstPeliculas) {
+		this.lstPeliculas = lstPeliculas;
+	}
 
 }
+	
+	  /*
+	    Casos de uso :
+		+ agregarPelicula (String pelicula) : boolean
+		Si la película existe en la lista lanzar la excepción
+		
+		+ traerPelicula (int idPelicula) : Pelicula
+		Si la película no existe devolver null
+		
+		+ traerPelicula (String partePelicula) : Pelicula //todas las películas que contengan en su
+		título el string partePelicula
+		
+		+ modificarPelicula (int idPelicula, String pelicula) :
+		Modificar la película traerPelicula por id, si no existe la película lanzar la excepción, de lo
+		contrario setPelicula con el parámetro pelicula .
+		
+		+ eliminarPelicula (int idPelicula): boolean
+		Eliminar la película: traerPelicula por su id, si no existe la película lanzar la excepción, de lo
+		contrario eliminar el elemento de la lista (remove)
+	 */
+
--- src/modelo/Pelicula.java
+++ src/modelo/Pelicula.java
@@ -2,4 +2,29 @@ package modelo;
 
 public class Pelicula {
 
+	private int idPelicula;
+	private String pelicula;
+
+	public Pelicula(int idPelicula, String pelicula) {
+		super();
+		this.idPelicula = idPelicula;
+		this.pelicula = pelicula;
+	}
+
+	public int getIdPelicula() {
+		return idPelicula;
+	}
+
+	public String getPelicula() {
+		return pelicula;
+	}
+
+	public void setIdPelicula(int idPelicula) {
+		this.idPelicula = idPelicula;
+	}
+
+	public void setPelicula(String pelicula) {
+		this.pelicula = pelicula;
+	}
+
 }
